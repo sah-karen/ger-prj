f7eee19edd0da24cc61fa5972faff960bc1df54d
karen <sahkaren@gmail.com>
2022-03-09 19:23:41 +0400

    Added test of login page and locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,23 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 #pytest -s -v --language=es .\test_main_page.py
 #pytest -v --tb=line --language=en test_main_page.py
 
 import time
 
-link = "http://selenium1py.pythonanywhere.com/"
+#link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
 
     page.open()
     page.should_be_login_link()
-    time.sleep(5)
-    #page.go_to_login_page()
+    page.go_to_login_page()
+    time.sleep(2)
+    pageLogin = LoginPage(browser, link)
+    pageLogin.should_be_login_page()
 
 # def test_add_to_cart(browser):
 #     page = ProductPage(url="", browser)   # инициализируем объект Page Object
